07d30fba06ecc47e0aee9c6417d5309e7f66b3ca
Christian Simeu <christian.kamgang.simeu@umontreal.ca>
2022-02-21 18:58:27 -0500

    Add java script

---
+++ src/applications/java.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Install fedora commons repository
+
+
+
+function java_install() 
+{
+    set -e
+    local FORCE=0
+    local IS_DEFAULT=0
+    local version=
+    # echo $@
+
+    local _parameters=
+    read_application_arguments $@ 
+    if [ -n "$_parameters" ]; then set $_parameters; fi
+
+    plateform=`plateform`
+
+    case $plateform in 
+        redhat)
+			if [[ $OS_VERSION =~ 6 ]]; then 
+                install -y java-1.8.0-openjdk-devel; 
+            else 
+                install -y java-11-openjdk-devel; 
+            fi
+            ;;
+        debian)
+            install -y default-jdk
+        ;;
+        *)
+            echo ">> Noy implemented script for plateform: $plateform"
+        ;;
+    esac
+}
+
+
