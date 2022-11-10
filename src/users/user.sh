983edeb7a01cd75f4925d8f227ef738c3c61e34d
Christian Simeu <christian.kamgang.simeu@umontreal.ca>
2022-11-10 11:45:06 -0500

    @update

--- src/users/user.sh
+++ src/users/user.sh
@@ -99,7 +99,7 @@ function user_add()
             alpine) 
                 if [ -n "$home" ]; then home="-h $home"; fi
                 echo "adduser -D --shell /bin/bash $uid $home ${username}"
-                adduser -D --shell /bin/bash $uid $home ${username} --non-unique
+                adduser -D --shell /bin/bash $uid $home ${username}
                 # moduser --shell /bin/bash $uid ${username};
             ;;
             *) 
