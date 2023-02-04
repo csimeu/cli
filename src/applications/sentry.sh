58b988e57b08807b8cb5ccbb9a47c3efc18ea4b6
Christian Simeu <christian.kamgang.simeu@umontreal.ca>
2023-02-04 21:39:21 +0000

    @updates

--- src/applications/sentry.sh
+++ src/applications/sentry.sh
@@ -21,7 +21,7 @@ function sentry_install()
     name="${name:-$appName}"
     data_dir="${data_dir:-/var/lib/$name}"
     home_dir="${home_dir:-/usr/share/$name}"
-
+    
     # python_install
     install build-essential libxslt1-dev gcc libffi-dev libjpeg-dev libxml2-dev libxslt-dev libyaml-dev libpq-dev supervisor
     install libxml2 libxmlsec1 pkg-config xmlsec1 libxmlsec1-dev
@@ -29,9 +29,11 @@ function sentry_install()
     # execute mkdir -p $home_dir
 
     if ! getent passwd $name > /dev/null 2>&1; then
-        sudo groupadd --system $name
-        sudo useradd -d $home_dir -r -s /bin/false -g $name $name
+        execute groupadd --system $name
+        execute useradd -d $home_dir -r -s /bin/false -g $name $name
     fi
+
+    sudo mkdir -p $home_dir $data_dir && chown $name:$name -R $home_dir $data_dir
 
     if [[ -n "$http_proxy" ]]; then
         sudo -u $name pip install --proxy $http_proxy -U sentry
