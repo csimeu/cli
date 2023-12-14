09557124541416ffa68a2ef29c81ee1486be2a9a
Christian Simeu <christian.kamgang.simeu@umontreal.ca>
2023-12-14 16:24:11 -0500

    Update ...

--- src/applications/nginx.sh
+++ src/applications/nginx.sh
@@ -3,7 +3,6 @@
 
 nginx_install() {
     install nginx
-    
-    if [[ -n "$ADMIN_USER" && $(getent passwd $ADMIN_USER)  ]]; then sudo usermod -aG nginx $ADMIN_USER; fi
+    # if [[ -n "$ADMIN_USER" && $(getent passwd $ADMIN_USER)  ]]; then sudo usermod -aG nginx $ADMIN_USER; fi
 }
 
